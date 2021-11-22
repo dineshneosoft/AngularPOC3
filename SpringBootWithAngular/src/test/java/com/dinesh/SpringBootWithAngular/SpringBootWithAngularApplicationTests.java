f732cbeb7d0c1fbee0b2628994f047c41969e82f
Dinesh <dineshneosoft@gmail.com>
2021-11-22 22:44:10 +0530

    First Commit

---
+++ SpringBootWithAngular/src/test/java/com/dinesh/SpringBootWithAngular/SpringBootWithAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinesh.SpringBootWithAngular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
